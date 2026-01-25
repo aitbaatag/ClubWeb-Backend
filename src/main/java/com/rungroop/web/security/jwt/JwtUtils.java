5df8b3e0d9de37c7d6a5bdad337f7e1856aaa454
Ait Baatag <aitbaatagkhalid@gmail.com>
2026-01-25 09:03:59 +0100

    Add JWT authentication support with configuration and utility methods

--- src/main/java/com/rungroop/web/security/jwt/JwtUtils.java
+++ src/main/java/com/rungroop/web/security/jwt/JwtUtils.java
@@ -1,4 +1,42 @@
 package com.rungroop.web.security.jwt;
 
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.SignatureAlgorithm;
+import io.jsonwebtoken.io.Decoders;
+import io.jsonwebtoken.security.Keys;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.stereotype.Component;
+
+import java.security.Key;
+import java.util.Date;
+import java.util.HashMap;
+import java.util.Map;
+
+@Component
 public class JwtUtils {
+
+    @Value("${app.jwt.secret}")
+    private  String SECRET;
+    @Value("${app.jwt.expiration}")
+    private  String EXEPIRATION;
+
+    public String generateToken(UserDetails userDetails) {
+        Map<String, Object> extraClaims = new HashMap<>();
+        return createToken(extraClaims, userDetails);
+    }
+    public String createToken(Map<String, Object> extraClaims, UserDetails userDetails) {
+        return Jwts.builder()
+                .setClaims(extraClaims)
+                .setSubject(userDetails.getUsername())
+                .setIssuedAt(new Date(System.currentTimeMillis()))
+                .setExpiration(new Date(System.currentTimeMillis() + Long.parseLong(EXEPIRATION)))
+                .signWith(getSignInKey(), SignatureAlgorithm.HS256)
+                .compact();
+    }
+
+    private Key getSignInKey() {
+        byte[] keyBytes = Decoders.BASE64.decode(SECRET);
+        return Keys.hmacShaKeyFor(keyBytes);
+    }
 }
